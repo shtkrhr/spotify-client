722723f2f0d636ec3b9da36068e18525e350dd3e
Shota Kurihara <shotath911@gmail.com>
2019-02-04 18:48:38 +0900

    画面遷移時にtopへスクロール

--- src/app/pages/app.component.ts
+++ src/app/pages/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'sp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-spotify';
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(_ => window.scroll(0, 0));
+  }
 }
