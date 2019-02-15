87a98330f7f10c296392bd08755559476ed92a8d
Shota Kurihara <shotath911@gmail.com>
2019-02-15 14:54:42 +0900

    Page title / readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  clientId: '05478059b83d4a38b0dfcf6bcd8061fd',
+  clientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   rootUrl: 'http://localhost:4200',
 };
 
